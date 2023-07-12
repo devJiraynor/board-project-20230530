d8bf674e643e1d03f4d47a48e00e66919a548ba3
jiraynor <jiraynor@naver.com>
2023-07-12 15:21:32 +0900

    2023.07.12. 15:20

---
+++ front/src/mocks/index.ts
@@ -0,0 +1,13 @@
+//! 게시판 리스트 아이템 목(임시) 데이터 //
+export const boardListItemMock = {
+  writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+  writerNickName: '안녕하세요나는주코야키',
+  writeDate: '2023. 07. 12.',
+  boardTitle: '오늘 점심 뭐먹지?',
+  boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  boardImage: 'http://cmsimg.newstomato.com/news/8/2021/02/2021-02-17/202102160856304393170.jpg',
+  commentCount: 10,
+  likeCount: 10,
+  viewCount: 10,
+  boardNumber: 1,
+}
