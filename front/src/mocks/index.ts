9c6135979eb57cfaa68ffa6d0d9db5ef6425b80c
jiraynor <jiraynor@naver.com>
2023-07-12 18:05:33 +0900

    2023.07.12. 18:20

--- front/src/mocks/index.ts
+++ front/src/mocks/index.ts
@@ -24,3 +24,10 @@ export const top3ListItemMock = {
   viewCount: 10,
   boardNumber: 1,
 }
+
+export const commentListItemMock = {
+  writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+  writerNickName: '안녕하세요나는주코야키',
+  writeTime: '3분전',
+  comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+}
