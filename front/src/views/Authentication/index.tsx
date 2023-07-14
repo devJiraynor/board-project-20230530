748199b97dbb1df6576481fa7468545790404148
jiraynor <jiraynor@naver.com>
2023-07-14 18:06:27 +0900

    2023.07.14. 18:00

--- front/src/views/Authentication/index.tsx
+++ front/src/views/Authentication/index.tsx
@@ -8,6 +8,12 @@ export default function Authentication() {
   const [view, setView] = useState<'sign-in' | 'sign-up'>('sign-in');
 
   const SignInCard = () => {
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [error, setError] = useState<boolean>(true);
+
+    const onPasswordIconClickHandler = () => {
+      setShowPassword(!showPassword);
+    }
 
     const onSignUpClickHandler = () => {
       setView('sign-up');
@@ -20,11 +26,16 @@ export default function Authentication() {
             <div className='auth-card-top-text'>로그인</div>
           </div>
           <div className='auth-card-top-input-container'>
-            <InputBox label='이메일 주소' type='text' placeholder='이메일 주소를 입력해 주세요.' />
-            <InputBox label='비밀번호' type='password' placeholder='비밀번호를 입력해 주세요.' icon={INPUT_ICON.OFF} />
+            <InputBox label='이메일 주소' type='text' placeholder='이메일 주소를 입력해 주세요.' error={error} />
+            <InputBox label='비밀번호' type={showPassword ? 'text' : 'password'} placeholder='비밀번호를 입력해 주세요.' icon={showPassword ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordIconClickHandler} error={error} />
           </div>
         </div>
         <div className='auth-card-bottom'>
+          {error && (
+            <div className='auth-card-bottom-error-message'>
+              {'이메일 주소 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.'}
+            </div>
+          )}
           <div className='auth-card-bottom-button'>로그인</div>
           <div className='auth-card-bottom-text'>
             신규 사용자이신가요? <span className='auth-emphasis' onClick={onSignUpClickHandler}>회원가입</span>
@@ -39,6 +50,11 @@ export default function Authentication() {
     const [page, setPage] = useState<1 | 2>(1);
     const [showPassword, setShowPassword] = useState<boolean>(false);
     const [showPasswordCheck, setShowPassowrdCheck] = useState<boolean>(false);
+
+    const [emailPatternError, setEmailPatternError] = useState<boolean>(false);
+    const [emailDuplicationError, setEmailDuplicationError] = useState<boolean>(false);
+    const [passwordError, setPasswordError] = useState<boolean>(true);
+    const [passwordCheckError, setPasswordCheckError] = useState<boolean>(true);
 
     const onPasswordIconClickHandler = () => {
       setShowPassword(!showPassword);
@@ -67,9 +83,9 @@ export default function Authentication() {
           <div className='auth-card-top-input-container'>
             {page === 1 ? (
               <>
-                <InputBox label='이메일 주소*' type='text' placeholder='이메일 주소를 입력해주세요.' />
-                <InputBox label='비밀번호*' type={showPassword ? 'text' : 'password'} placeholder='비밀번호를 입력해주세요.' icon={showPassword ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordIconClickHandler} />
-                <InputBox label='비밀번호 확인*' type={showPasswordCheck ? 'text' : 'password'} placeholder='비밀번호를 다시 입력해주세요.' icon={showPasswordCheck ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordCheckIconClickHandler} />
+                <InputBox label='이메일 주소*' type='text' placeholder='이메일 주소를 입력해주세요.' error={emailPatternError || emailDuplicationError} helper={emailPatternError ? '이메일 주소 포맷이 맞지않습니다.' : emailDuplicationError ? '중복되는 이메일 주소입니다.' : '' }/>
+                <InputBox label='비밀번호*' type={showPassword ? 'text' : 'password'} placeholder='비밀번호를 입력해주세요.' icon={showPassword ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordIconClickHandler} error={passwordError} helper={passwordError ? '비밀번호는 8자 이상 입력해주세요.' : ''} />
+                <InputBox label='비밀번호 확인*' type={showPasswordCheck ? 'text' : 'password'} placeholder='비밀번호를 다시 입력해주세요.' icon={showPasswordCheck ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordCheckIconClickHandler} error={passwordCheckError} helper={passwordCheckError ? '비밀번호가 일치하지않습니다.' : ''} />
               </>
             ) : (
               <>
