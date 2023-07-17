4cc263f3b950f4b110fd8cda7dcc7d07133b446e
jiraynor <jiraynor@naver.com>
2023-07-17 17:19:30 +0900

    2023.07.17. 17:20

--- front/src/mocks/index.ts
+++ front/src/mocks/index.ts
@@ -25,6 +25,45 @@ export const top3ListItemMock = {
   boardNumber: 1,
 }
 
+export const top3ListMock = [
+  {
+    writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+    writerNickName: '안녕하세요나는주코야키',
+    writeDate: '2023. 07. 12.',
+    boardTitle: '오늘 점심 뭐먹지?',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    boardImage: 'https://png.pngtree.com/thumb_back/fh260/background/20210207/pngtree-texture-black-minimalistic-background-image_556936.jpg',
+    commentCount: 10,
+    likeCount: 10,
+    viewCount: 10,
+    boardNumber: 1,
+  },
+  {
+    writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+    writerNickName: '안녕하세요나는주코야키',
+    writeDate: '2023. 07. 12.',
+    boardTitle: '오늘 점심 뭐먹지?',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    boardImage: 'https://png.pngtree.com/thumb_back/fh260/background/20210207/pngtree-texture-black-minimalistic-background-image_556936.jpg',
+    commentCount: 10,
+    likeCount: 10,
+    viewCount: 10,
+    boardNumber: 1,
+  },
+  {
+    writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+    writerNickName: '안녕하세요나는주코야키',
+    writeDate: '2023. 07. 12.',
+    boardTitle: '오늘 점심 뭐먹지?',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    boardImage: 'https://png.pngtree.com/thumb_back/fh260/background/20210207/pngtree-texture-black-minimalistic-background-image_556936.jpg',
+    commentCount: 10,
+    likeCount: 10,
+    viewCount: 10,
+    boardNumber: 1,
+  }
+];
+
 export const commentListItemMock = {
   writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
   writerNickName: '안녕하세요나는주코야키',
