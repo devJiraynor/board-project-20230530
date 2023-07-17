db67326951a61d1dff9d443a2fe840f905e258e7
jiraynor <jiraynor@naver.com>
2023-07-17 15:22:52 +0900

    2023.07.17. 15:20

--- front/src/mocks/index.ts
+++ front/src/mocks/index.ts
@@ -31,3 +31,8 @@ export const commentListItemMock = {
   writeTime: '3분전',
   comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 }
+
+export const signInMock = {
+  email: 'email@email.com',
+  password: 'P!ssw0rd'
+}
--- front/src/views/Authentication/index.tsx
+++ front/src/views/Authentication/index.tsx
@@ -2,14 +2,21 @@ import React, { useState } from 'react';
 import './style.css';
 import InputBox from 'src/components/InputBox';
 import { INPUT_ICON } from 'src/constants';
+import { signInMock } from 'src/mocks';
+import { useNavigate } from 'react-router-dom';
 
 export default function Authentication() {
 
   const [view, setView] = useState<'sign-in' | 'sign-up'>('sign-in');
 
+  const navigator = useNavigate();
+
   const SignInCard = () => {
     const [showPassword, setShowPassword] = useState<boolean>(false);
-    const [error, setError] = useState<boolean>(true);
+    const [error, setError] = useState<boolean>(false);
+
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
     const onPasswordIconClickHandler = () => {
       setShowPassword(!showPassword);
@@ -19,6 +26,14 @@ export default function Authentication() {
       setView('sign-up');
     }
 
+    const onSignInButtonClickHandler = () => {
+      if (email !== signInMock.email || password !== signInMock.password) {
+        setError(true);
+        return;
+      }
+      navigator('/');
+    }
+
     return (
       <div className='auth-card'>
         <div className='auth-card-top'>
@@ -26,8 +41,8 @@ export default function Authentication() {
             <div className='auth-card-top-text'>로그인</div>
           </div>
           <div className='auth-card-top-input-container'>
-            <InputBox label='이메일 주소' type='text' placeholder='이메일 주소를 입력해 주세요.' error={error} />
-            <InputBox label='비밀번호' type={showPassword ? 'text' : 'password'} placeholder='비밀번호를 입력해 주세요.' icon={showPassword ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordIconClickHandler} error={error} />
+            <InputBox label='이메일 주소' type='text' placeholder='이메일 주소를 입력해 주세요.' error={error} value={email} setValue={setEmail} />
+            <InputBox label='비밀번호' type={showPassword ? 'text' : 'password'} placeholder='비밀번호를 입력해 주세요.' icon={showPassword ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordIconClickHandler} error={error} value={password} setValue={setPassword} />
           </div>
         </div>
         <div className='auth-card-bottom'>
@@ -36,7 +51,7 @@ export default function Authentication() {
               {'이메일 주소 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.'}
             </div>
           )}
-          <div className='auth-card-bottom-button'>로그인</div>
+          <div className='auth-card-bottom-button' onClick={onSignInButtonClickHandler}>로그인</div>
           <div className='auth-card-bottom-text'>
             신규 사용자이신가요? <span className='auth-emphasis' onClick={onSignUpClickHandler}>회원가입</span>
           </div>
@@ -46,7 +61,6 @@ export default function Authentication() {
   }
 
   const SignUpCard = () => {
-
     const [page, setPage] = useState<1 | 2>(1);
     const [showPassword, setShowPassword] = useState<boolean>(false);
     const [showPasswordCheck, setShowPassowrdCheck] = useState<boolean>(false);
@@ -56,9 +70,18 @@ export default function Authentication() {
     const [passwordError, setPasswordError] = useState<boolean>(false);
     const [passwordCheckError, setPasswordCheckError] = useState<boolean>(false);
 
-    const [nicknameError, setNicknameError] = useState<boolean>(true);
-    const [telNumberError, setTelNumberError] = useState<boolean>(true);
-    const [addressError, setAddressError] = useState<boolean>(true);
+    const [nicknameError, setNicknameError] = useState<boolean>(false);
+    const [telNumberError, setTelNumberError] = useState<boolean>(false);
+    const [addressError, setAddressError] = useState<boolean>(false);
+
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [passwordCheck, setPasswordCheck] = useState<string>('');
+
+    const [nickname, setNickname] = useState<string>('');
+    const [telNumber, setTelNumber] = useState<string>('');
+    const [address, setAddress] = useState<string>('');
+    const [addressDetail, setAddressDetail] = useState<string>('');
 
     const onPasswordIconClickHandler = () => {
       setShowPassword(!showPassword);
@@ -69,8 +92,21 @@ export default function Authentication() {
     }
 
     const onButtonClickHandler = () => {
-      if (page === 1) setPage(2);
-      else setPage(1);
+      if (page === 1) {
+        setPasswordError(password.length < 8);
+        setPasswordCheckError(password !== passwordCheck);
+        if (!passwordError && !passwordCheckError) setPage(2);
+      } else {
+        if (!nickname) {
+          setNicknameError(true);
+          return;
+        }
+        if (!address) {
+          setAddressError(true);
+          return;
+        }
+        setView('sign-in');
+      }
     }
 
     const onSignInClickHandler = () => {
@@ -87,16 +123,16 @@ export default function Authentication() {
           <div className='auth-card-top-input-container'>
             {page === 1 ? (
               <>
-                <InputBox label='이메일 주소*' type='text' placeholder='이메일 주소를 입력해주세요.' error={emailPatternError || emailDuplicationError} helper={emailPatternError ? '이메일 주소 포맷이 맞지않습니다.' : emailDuplicationError ? '중복되는 이메일 주소입니다.' : '' }/>
-                <InputBox label='비밀번호*' type={showPassword ? 'text' : 'password'} placeholder='비밀번호를 입력해주세요.' icon={showPassword ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordIconClickHandler} error={passwordError} helper={passwordError ? '비밀번호는 8자 이상 입력해주세요.' : ''} />
-                <InputBox label='비밀번호 확인*' type={showPasswordCheck ? 'text' : 'password'} placeholder='비밀번호를 다시 입력해주세요.' icon={showPasswordCheck ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordCheckIconClickHandler} error={passwordCheckError} helper={passwordCheckError ? '비밀번호가 일치하지않습니다.' : ''} />
+                <InputBox label='이메일 주소*' type='text' placeholder='이메일 주소를 입력해주세요.' error={emailPatternError || emailDuplicationError} helper={emailPatternError ? '이메일 주소 포맷이 맞지않습니다.' : emailDuplicationError ? '중복되는 이메일 주소입니다.' : '' } value={email} setValue={setEmail} />
+                <InputBox label='비밀번호*' type={showPassword ? 'text' : 'password'} placeholder='비밀번호를 입력해주세요.' icon={showPassword ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordIconClickHandler} error={passwordError} helper={passwordError ? '비밀번호는 8자 이상 입력해주세요.' : ''} value={password} setValue={setPassword} />
+                <InputBox label='비밀번호 확인*' type={showPasswordCheck ? 'text' : 'password'} placeholder='비밀번호를 다시 입력해주세요.' icon={showPasswordCheck ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordCheckIconClickHandler} error={passwordCheckError} helper={passwordCheckError ? '비밀번호가 일치하지않습니다.' : ''} value={passwordCheck} setValue={setPasswordCheck} />
               </>
             ) : (
               <>
-                <InputBox label='닉네임*' type='text' placeholder='닉네임을 입력해주세요.' error={nicknameError} helper={nicknameError ? '닉네임을 입력해주세요.' : ''} />
-                <InputBox label='핸드폰 번호*' type='text' placeholder='핸드폰 번호를 입력해주세요.' error={telNumberError} helper={telNumberError ? '숫자만 입력해주세요.' : ''} />
-                <InputBox label='주소*' type='text' placeholder='우편번호 찾기' icon={INPUT_ICON.ARROW} error={addressError} helper={addressError ? '우편번호를 선택해주세요.' : ''} />
-                <InputBox label='상세 주소*' type='text' placeholder='상세 주소를 입력해주세요.' />
+                <InputBox label='닉네임*' type='text' placeholder='닉네임을 입력해주세요.' error={nicknameError} helper={nicknameError ? '닉네임을 입력해주세요.' : ''} value={nickname} setValue={setNickname} />
+                <InputBox label='핸드폰 번호*' type='text' placeholder='핸드폰 번호를 입력해주세요.' error={telNumberError} helper={telNumberError ? '숫자만 입력해주세요.' : ''} value={telNumber} setValue={setTelNumber} />
+                <InputBox label='주소*' type='text' placeholder='우편번호 찾기' icon={INPUT_ICON.ARROW} error={addressError} helper={addressError ? '우편번호를 선택해주세요.' : ''} value={address} setValue={setAddress} />
+                <InputBox label='상세 주소*' type='text' placeholder='상세 주소를 입력해주세요.' value={addressDetail} setValue={setAddressDetail} />
               </>
             )}
           </div>
