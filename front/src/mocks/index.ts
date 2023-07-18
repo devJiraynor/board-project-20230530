0b9cd45468f62c6afc83ee53682d508fa7003618
jiraynor <jiraynor@naver.com>
2023-07-18 18:15:20 +0900

    2023.07.18. 18:20

--- front/src/mocks/index.ts
+++ front/src/mocks/index.ts
@@ -75,7 +75,96 @@ export const currentBoardListMock = [
   }
 ];
 
+export const searchBoardListMock = [
+  {
+    writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+    writerNickName: '안녕하세요나는주코야키',
+    writeDate: '2023. 07. 12.',
+    boardTitle: '오늘 점심 뭐먹지?',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    boardImage: 'http://cmsimg.newstomato.com/news/8/2021/02/2021-02-17/202102160856304393170.jpg',
+    commentCount: 10,
+    likeCount: 10,
+    viewCount: 10,
+    boardNumber: 1,
+  },
+  {
+    writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+    writerNickName: '안녕하세요나는주코야키',
+    writeDate: '2023. 07. 12.',
+    boardTitle: '오늘 점심 뭐먹지?',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    boardImage: 'http://cmsimg.newstomato.com/news/8/2021/02/2021-02-17/202102160856304393170.jpg',
+    commentCount: 11,
+    likeCount: 11,
+    viewCount: 11,
+    boardNumber: 2,
+  },
+  {
+    writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+    writerNickName: '안녕하세요나는주코야키',
+    writeDate: '2023. 07. 12.',
+    boardTitle: '오늘 점심 뭐먹지?',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    boardImage: 'http://cmsimg.newstomato.com/news/8/2021/02/2021-02-17/202102160856304393170.jpg',
+    commentCount: 12,
+    likeCount: 12,
+    viewCount: 12,
+    boardNumber: 3,
+  },
+  {
+    writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+    writerNickName: '안녕하세요나는주코야키',
+    writeDate: '2023. 07. 12.',
+    boardTitle: '오늘 점심 뭐먹지?',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    boardImage: 'http://cmsimg.newstomato.com/news/8/2021/02/2021-02-17/202102160856304393170.jpg',
+    commentCount: 13,
+    likeCount: 13,
+    viewCount: 13,
+    boardNumber: 4,
+  },
+  {
+    writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+    writerNickName: '안녕하세요나는주코야키',
+    writeDate: '2023. 07. 12.',
+    boardTitle: '오늘 점심 뭐먹지?',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    boardImage: 'http://cmsimg.newstomato.com/news/8/2021/02/2021-02-17/202102160856304393170.jpg',
+    commentCount: 14,
+    likeCount: 14,
+    viewCount: 14,
+    boardNumber: 5,
+  },
+  {
+    writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+    writerNickName: '안녕하세요나는주코야키',
+    writeDate: '2023. 07. 12.',
+    boardTitle: '오늘 점심 뭐먹지?',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    boardImage: 'http://cmsimg.newstomato.com/news/8/2021/02/2021-02-17/202102160856304393170.jpg',
+    commentCount: 13,
+    likeCount: 13,
+    viewCount: 13,
+    boardNumber: 4,
+  },
+  {
+    writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
+    writerNickName: '안녕하세요나는주코야키',
+    writeDate: '2023. 07. 12.',
+    boardTitle: '오늘 점심 뭐먹지?',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    boardImage: 'http://cmsimg.newstomato.com/news/8/2021/02/2021-02-17/202102160856304393170.jpg',
+    commentCount: 14,
+    likeCount: 14,
+    viewCount: 14,
+    boardNumber: 5,
+  }
+];
+
 export const popularWordListMock = ['안녕', '반가워', '아침', '점심', '저녁', '퇴근하고싶다.'];
+
+export const relationWordListMock = ['안녕', '반가워', '아침', '점심', '저녁', '퇴근하고싶다.'];
 
 export const top3ListItemMock = {
   writerProfileImage: 'https://cdn.thebilliards.kr/news/photo/202305/20293_20326_517.jpg',
