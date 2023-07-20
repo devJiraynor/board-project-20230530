39a69469aa5aa20daca3a2cb4c471d264d351c66
jiraynor <jiraynor@naver.com>
2023-07-20 15:21:16 +0900

    2023.07.20. 15:20

--- front/src/views/Main/index.tsx
+++ front/src/views/Main/index.tsx
@@ -47,25 +47,38 @@ export default function Main() {
     )
   }
 
+  //          component          //
+  // description: 메인 화면의 하단 //
   const MainBottom = () => {
+
+    //           state          //
+    // description: 최신 게시물 리스트 상태 //
     const [currentList, setCurrentList] = useState<CurrentListResponseDto[]>([]);
+    // description: 인기 검색어 리스트 상태 //
     const [popularList, setPopularList] = useState<string[]>([]);
 
+    // description: 페이지네이션 관련 상태 및 함수 //
     const { totalPage, currentPage, currentSection, onPageClickHandler, onNextClickHandler, onPreviousClickHandler, changeSection } = usePagination();
 
+    //          event handler          //
+    // description: 인기 검색어 클릭 이벤트 //
     const onPopularClickHandler = (word: string) => {
       navigator(`/search/${word}`);
     }
 
+    //          effect          //
+    // description: 첫 시작 시 인기 검색어 리스트 불러오기 //
+    useEffect(() => {
+      if (!popularList.length) setPopularList(popularWordListMock);
+    }, []);
+
+    // description: 현재 섹션이 바뀔 때마다 페이지 리스트 변경 및 최신 게시물 불러오기 //
     useEffect(() => {
       changeSection(72);
       if (!currentList.length) setCurrentList(currentBoardListMock);
     }, [currentSection]);
 
-    useEffect(() => {
-      if (!popularList.length) setPopularList(popularWordListMock);
-    }, []);
-
+    //          render          //
     return (
       <div className='main-bottom'>
         <div className='main-bottom-text'>최신 게시물</div>
@@ -93,6 +106,7 @@ export default function Main() {
     )
   }
 
+  //          render          //
   return (
     <div id='main-wrapper'>
       <MainTop />
--- front/src/views/Search/index.tsx
+++ front/src/views/Search/index.tsx
@@ -8,25 +8,29 @@ import { COUNT_BY_PAGE } from 'src/constants';
 import Pagination from 'src/components/Pagination';
 import { usePagination } from 'src/hooks';
 
+//          component          //
+// description: 검색 화면 //
 export default function Search() {
 
-  const navigator = useNavigate();
-
+  //          state          //
+  // description: 검색어 path parameter 상태 //
   const { searchWord } = useParams();
-
+  // description: 게시물 수를 저장하는 상태 //
   const [boardCount, setBoardCount] = useState<number>(0);
-
+  // description: 전체 게시물 리스트 상태 //
   const [searchList, setSearchList] = useState<SearchListResponseDto[]>([]);
+  // description: 현재 페이지에서 보여줄 게시물 리스트 상태 //
   const [pageBoardList, setPageBoardList] = useState<SearchListResponseDto[]>([]);
-
+  // description: 연관 검색어 리스트 상태 //
   const [relationList, setRelationList] = useState<string[]>([]);
 
+  // description: 페이지네이션과 관련된 상태 및 함수 //
   const { totalPage, currentPage, currentSection, onPageClickHandler, onNextClickHandler, onPreviousClickHandler, changeSection } = usePagination();
 
-  const onRelationClickHandler = (word: string) => {
-    navigator(`/search/${word}`);
-  }
-
+  //          function          //
+  // description: 페이지 이동을 위한 네비게이트 함수 //
+  const navigator = useNavigate();
+  // description: 현재 페이지의 게시물 리스트 분류 함수 //
   const getPageBoardList = () => {
     const lastIndex = 
       searchBoardListMock.length > COUNT_BY_PAGE * currentPage ? 
@@ -37,6 +41,14 @@ export default function Search() {
     setPageBoardList(pageBoardList);
   }
 
+  //          event handler          //
+  // description: 연관 검색어 클릭 이벤트 //
+  const onRelationClickHandler = (word: string) => {
+    navigator(`/search/${word}`);
+  }
+
+  //          effect          //
+  // description: 검색어 상태가 바뀔때 마다 해당 검색어의 검색 결과 불러오기 //
   useEffect(() => {
     setSearchList(searchBoardListMock);
     setBoardCount((searchWord as string).length);
@@ -47,14 +59,17 @@ export default function Search() {
     changeSection(searchBoardListMock.length);
   }, [searchWord]);
 
+  // description: 현재 색션이 바뀔때 마다 페이지 리스트 변경 //
   useEffect(() => {
     changeSection(searchBoardListMock.length);
   }, [currentSection]);
 
+  // description: 현재 페이지가 바뀔때 마다 검색 게시물 분류하기 //
   useEffect(() => {
     getPageBoardList();
   }, [currentPage]);
 
+  //          render          //
   return (
     <div id='search-wrapper'>
       <div className='search-text-container'>
@@ -63,25 +78,38 @@ export default function Search() {
         <div className='search-text-emphasis'>{boardCount}</div>
       </div>
       <div className='search-container'>
-        <div className='search-board-list'>
-          {pageBoardList.map((item) => (<BoardListItem item={item} />))}
-        </div>
+        
+        {boardCount ? (
+          <div className='search-board-list'>
+            {pageBoardList.map((item) => (<BoardListItem item={item} />))}
+          </div>
+        ) : (
+          <div className='search-board-list-nothing'>검색 결과가 없습니다.</div>
+        )}
+
         <div className='search-relation-box'>
           <div className='search-relation-card'>
             <div className='search-relation-text'>관련 검색어</div>
-            <div className='search-relation-list'>
-              {relationList.map((item) => (<div className='relation-chip' onClick={() => onRelationClickHandler(item)}>{item}</div>))}
-            </div>
+
+            {relationList.length ? (
+              <div className='search-relation-list'>
+                {relationList.map((item) => (<div className='relation-chip' onClick={() => onRelationClickHandler(item)}>{item}</div>))}
+              </div>
+            ) : (
+              <div className='search-relation-list-nothing'>관련 검색어가 없습니다.</div>
+            )}
           </div>
         </div>
       </div>
-      <Pagination 
-        totalPage={totalPage} 
-        currentPage={currentPage} 
-        onPageClickHandler={onPageClickHandler} 
-        onNextClickHandler={onNextClickHandler} 
-        onPreviousClickHandler={onPreviousClickHandler} 
-      />
+      {boardCount !== 0 && (
+        <Pagination 
+          totalPage={totalPage} 
+          currentPage={currentPage} 
+          onPageClickHandler={onPageClickHandler} 
+          onNextClickHandler={onNextClickHandler} 
+          onPreviousClickHandler={onPreviousClickHandler} 
+        />
+      )}
     </div>
   )
 }
